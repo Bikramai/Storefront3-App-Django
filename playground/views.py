86c1548e3593e0b68649de777b4249e158d1e583
bikramia <bikram.rhce@gmail.com>
2024-02-14 15:30:43 -0500

    Send-Mail

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,11 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+        send_mail('subject', 'message', 'info@bikrambuy.com',
+                  ['bob@bikrambuy.com'])
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html', {'name': 'Bikram'})
