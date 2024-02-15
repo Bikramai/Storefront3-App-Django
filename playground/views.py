4db6b1702f2dcdc8bf4cafe9060d4747b1c69050
bikramia <bikram.rhce@gmail.com>
2024-02-15 11:48:02 -0500

    Send Templated Emails

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,15 @@
 from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
     try:
-        message = EmailMessage('subject', 'message', 'from@mangbuy.com', ['Hang@mangbuy.com'])
-        message.attach_file('playground/static/images/git.png')
-        message.send()
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Bikram'}
+        )
+        message.send(['mangni@bikrambuy.com'])
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Bikram'})
